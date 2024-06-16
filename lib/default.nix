a2ce7fcc4dee4e1653dd52c9d89bb098a0b04ed4
Hallam Smith <me@hcssmith.com>
2024-06-16 18:24:14 +0100

    "wezterm builder"

--- lib/default.nix
+++ lib/default.nix
@@ -6,5 +6,6 @@
   mkNeovim = import ./neovim {inherit nix-colors;};
   mkTmux = import ./tmux {inherit nix-colors;};
   mkNushell = import ./nushell {inherit nix-colors;};
+  mkWezterm = import ./wezterm {inherit nix-colors;};
   lua-utils = import ./utils/lua-utils.nix {pkgs = nixpkgs;};
 }
---
+++ lib/wezterm/default.nix
@@ -0,0 +1,30 @@
+{nix-colors}: {
+  pkgs,
+  wezterm ? pkgs.wezterm,
+  shell ? "nu",
+  config,
+  ...
+}: let
+  lua-utils = import ../utils/lua-utils.nix {inherit pkgs;};
+  inherit (lua-utils) toLuaObject;
+
+  mkConfig = {tabBar ? false}:
+    pkgs.writeText "wezterm.lua" (builtins.concatStringsSep "\n" [
+      "local wezterm = require('wezterm')"
+      "local config = wezterm.config_builder()"
+      "config.enable_tab_bar = ${toLuaObject tabBar}"
+      "config.font = wezterm.font_with_fallback {"
+      "'CMU Typewriter Text',"
+      "'FiraCode Mono',"
+      "}"
+      "config.default_prog = {'${shell}'}"
+      "return config"
+    ]);
+in
+  pkgs.runCommand wezterm.meta.mainProgram {
+    nativeBuildInputs = with pkgs; [makeWrapper];
+  } ''
+    mkdir -p $out/bin
+     makeWrapper ${wezterm}/bin/wezterm $out/bin/wezterm \
+    	--add-flags "--config-file ${mkConfig config}"
+  ''
